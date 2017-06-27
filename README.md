93c822d88a0d8ee3d587b833607eb58fc69c515e
Niko <niko.caron90@gmail.com>
2017-06-27 18:57:52 +0200

    Added morning, afternoon and evening greetings
    The browser will detect the time of day and display the appropriate
    greetings based off mornings, afternoons and evenings.

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ I've created this to add it to my portfolio's degign because I think it's import
 - Wrap the desired element in `autogreeter-wrap`.
 - Add the class `autogreeter` to desired element.
 - [optional] Add the class autogreeter-cursor to the same element and style it.
-- [optional] Change `autogreeterSettings.greetings` to add or remove desired languages.
+- [optional] Change `autogreeterSettings.default` to add or remove desired languages.
+- [optional] Change/remove `autogreeterSettings.morning`, `autogreeterSettings.afternoon`, `autogreeterSettings.evening`.
 - [optional] Change the speed settings as required. NOTE: Speed is in milliseconds, lowering the number means faster.
--- autogreeter.js
+++ autogreeter.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
   var autogreeterSettings = {
-    greetings: [
+    default: [
       'Hello', 'Bonjour', 'Hola', 'Hei', 'Hallo', 'こんにちは',
     ' Здравствуйте', '你好', 'Merhaba', 'Χαίρετε', '안녕하세요'
     ],
-    typeSpeed: 100,
-    eraseSpeed: 125,
+    morning: [
+      'Good Morning', 'Bonjour', 'Buonos Días', 'Hyvää Huomenta', 'Guten Morgen',
+      'おはようございます', 'Доброе утро', '早上好', 'Günaydın', 'Καλημέρα', '좋은 아침'
+    ],
+    afternoon: [
+      'Good Afternoon', 'Bonne Après-Midi', 'Buenas Tardes', 'Hyvää iltapäivää', 'Guten Nachmittag',
+      'こんにちは', 'Добрый день', '下午好', 'Tünaydın', 'Καλό απόγευμα', '안녕하세요'
+    ],
+    evening: [
+      'Good Evening', 'Bonsoir', 'Buena Noches', 'Hyvää iltaa', 'Guten Abend',
+      'こんばんは', 'Добрый вечер', '晚上好', 'İyi akşamlar', 'Καλό απόγευμα', '안녕하세요'
+    ],
+    typeSpeed: 90,
+    eraseSpeed: 100,
     cursorSpeed: 400,
     eraseDelay: 1600,
     restartDelay: 50,
   }
 
-  var unusedGreetingsList = resetGreetings();
+  var greetings = getGreetingsList();
+  var unusedGreetingsList = greetings.slice();
   var greetBox = document.querySelector('.autogreeter-greeting');
   var cursorInterval;
 
@@ -29,10 +42,26 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
   /*
-  ** Resets the list of greetings when all greetings have been used.
+  ** Gets the list of greetings based off the time of the day.
   */
-  function resetGreetings() {
-    return autogreeterSettings.greetings.slice();
+  function getGreetingsList() {
+    var date = new Date();
+    var hour = date.getHours();
+    var greetings;
+
+    if (hour >= 5 && hour < 12) {
+      greetings = autogreeterSettings.morning;
+    } else if (hour >= 12 && hour < 18) {
+      greetings = autogreeterSettings.afternoon;
+    } else if (hour >= 18 || hour < 5) {
+      greetings = autogreeterSettings.evening;
+    } else {
+      greetings = autogreeterSettings.default;
+    }
+
+    greetings = greetings || autogreeterSettings.default;
+
+    return greetings.slice();
   }
 
 
@@ -42,7 +71,7 @@ document.addEventListener('DOMContentLoaded', function() {
   */
   function getGreeting() {
     if (unusedGreetingsList.length === 0) {
-      unusedGreetingsList = resetGreetings();
+      unusedGreetingsList = greetings.slice();
     }
 
     var greetingsIndex = Math.floor(Math.random() * unusedGreetingsList.length);
@@ -56,11 +85,17 @@ document.addEventListener('DOMContentLoaded', function() {
   ** Displays the greeting to the screen, one letter at a time with the interval.
   ** Stops the interval and calls typeDecision on a setTimeout after the greeting is completely displayed.
   ** Starts a blinking cursor.
+  ** The first if statement is to add the space at the same time as letter to prevent a jank look.
   */
   function displayGreeting(greeting) {
     var i = 0;
 
     var typeInterval = setInterval(function() {
+      if (greeting[i] === ' ') {
+        greetBox.textContent += greeting[i];
+        i += 1;
+      }
+      
       greetBox.textContent += greeting[i];
       i += 1;
 
@@ -80,12 +115,19 @@ document.addEventListener('DOMContentLoaded', function() {
   /*
   ** Stops the cursor interval.
   ** Removes the greeting, one letter at a time with the interval.
+  ** The first if statement is to remove the space at the same time as letter to prevent a jank look.
   */
   function removeGreeting() {
     clearInterval(cursorInterval);
 
     var removeTypeInterval = setInterval(function() {
-      greetBox.textContent = greetBox.textContent.substr(0, greetBox.textContent.length - 1);
+      var toRemove = 1;
+
+      if (greetBox.textContent[greetBox.textContent.length - 1] === ' ') {
+        toRemove = 2;
+      }
+
+      greetBox.textContent = greetBox.textContent.substr(0, greetBox.textContent.length - toRemove);
 
       if (greetBox.textContent.length === 0) {
         clearInterval(removeTypeInterval);
